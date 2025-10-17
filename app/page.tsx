45e86cf00192b3be7821b4c4950ba0532ab4b3db
athazayyan <achmadatha@mhs.usk.ac.id>
2025-10-18 01:16:09 +0700

    chore: delete the redirect function

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,6 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect will be handled by middleware
-    // This is just a fallback loading state
-    router.push("/auth");
-  }, [router]);
 
   return (
    <div className="bg-gradienthijau">

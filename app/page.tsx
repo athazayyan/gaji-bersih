29fdf8e59b4eadffd6ff02a40f0409a92afc5cae
athazayyan <achmadatha@mhs.usk.ac.id>
2025-10-18 01:11:49 +0700

    chore: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
    <div className="bg-gradienthijau">
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <h1 className="text-3xl font-bold underline"></h1>
    </div>
   );
 }
